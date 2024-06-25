5a134739b214da58de07b0e073bd172e4a3ab0b8
Kirill <krikus.ms@gmail.com>
2024-06-25 13:32:32 +0300

    fixed imagesize

--- dist/kernel.js
+++ dist/kernel.js
@@ -2813,7 +2813,7 @@ function arrDepth(arr) {
   interpretate.contextExpand(g2d);
 
  //polyfill for symbols
- ["FaceForm", "CurrentValue", "FontColor", "Tiny", "VertexColors", "Antialiasing","Small", "Plot",  "ListLinePlot", "ListPlot", "Automatic", "Controls","All","TickLabels","FrameTicksStyle", "AlignmentPoint","AspectRatio","Axes","AxesLabel","AxesOrigin","AxesStyle","Background","BaselinePosition","BaseStyle","ColorOutput","ContentSelectable","CoordinatesToolOptions","DisplayFunction","Epilog","FormatType","Frame","FrameLabel","FrameStyle","FrameTicks","FrameTicksStyle","GridLines","GridLinesStyle","ImageMargins","ImagePadding","ImageSize","ImageSizeRaw","LabelStyle","Method","PlotLabel","PlotRange","PlotRangeClipping","PlotRangePadding","PlotRegion","PreserveImageOptions","Prolog","RotateLabel","Ticks","TicksStyle", "TransitionDuration"].map((name)=>{
+ ["FaceForm", "CurrentValue", "FontColor", "Tiny", "VertexColors", "Antialiasing","Small", "Plot",  "ListLinePlot", "ListPlot", "Automatic", "Controls","All","TickLabels","FrameTicksStyle", "AlignmentPoint","AspectRatio","Axes","AxesLabel","AxesOrigin","AxesStyle","Background","BaselinePosition","BaseStyle","ColorOutput","ContentSelectable","CoordinatesToolOptions","DisplayFunction","Epilog","FormatType","Frame","FrameLabel","FrameStyle","FrameTicks","FrameTicksStyle","GridLines","GridLinesStyle","ImageMargins","ImagePadding","ImageSize","ImageSizeRaw","Full","LabelStyle","Method","PlotLabel","PlotRange","PlotRangeClipping","PlotRangePadding","PlotRegion","PreserveImageOptions","Prolog","RotateLabel","Ticks","TicksStyle", "TransitionDuration"].map((name)=>{
   g2d[name] = () => name;
   //g2d[name].destroy = () => name;
   g2d[name].update = () => name;
@@ -2975,7 +2975,14 @@ function arrDepth(arr) {
     /**
      * @type {[Number, Number]}
      */
-    let ImageSize = await interpretate(options.ImageSize, env);
+    let ImageSize = await interpretate(options.ImageSize, {...env, context: g2d});
+    if (typeof ImageSize === 'number') {
+      if (ImageSize < 1) {
+        ImageSize = 10000.0 * ImageSize / 2.0;
+      }
+    } else if (typeof ImageSize === 'string'){
+      ImageSize = core.DefaultWidth;
+    }
     
     if (!ImageSize) {
       if (env.imageSize) {
@@ -6248,8 +6255,7 @@ function vectorAngle ([ux, uy], [vx, vy]) {
       centre.y = (centre.y + centre.height / 2);
     }
        
-    const rotation = "rotate(" + (degrees / Math.PI * 180.0) + ", " + 
-    (centre.x ) + ", " + (centre.y ) + ")";
+    const rotation = "rotate(" + (degrees / Math.PI * 180.0) + ", " + (centre.x ) + ", " + (centre.y ) + ")";
 
     var interpol_rotate = d3.interpolateString(env.local.rotation, rotation);
 
