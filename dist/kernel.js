d6a8e807b70042902faf2cf086395f25bd98aeed
Kirill <krikus.ms@gmail.com>
2024-06-30 19:40:22 +0200

    inset fix

--- dist/kernel.js
+++ dist/kernel.js
@@ -3819,21 +3819,71 @@ function arrDepth(arr) {
   g2d.Graphics.destroy = (args, env) => { console.error('Nothing to destroy...'); };
 
   g2d.Inset = async (args, env) => {
-    const co = await interpretate(args[1], env);
+    let pos = [0,0];
+    let size; 
+    
+    if (args.length > 1) pos = await interpretate(args[1], env);
+    let opos;
+
+    if (args.length > 2) opos = await interpretate(args[2], env);
+    if (args.length > 3) size = await interpretate(args[3], env);
 
     const group = env.svg.append('g');
-    await interpretate(args[0], {...env, inset: group});
-
+    const copy = {global: env.global, inset: group};
+   
+    const instance = new ExecutableObject('feinset-'+uuidv4(), copy, args[0]);
+    instance.assignScope(copy);
+  
+    await instance.execute();    
+  
     env.local.group = group;
+    const bbox = group.node().getBoundingClientRect();
+    if (!opos) {
+      opos = [bbox.width/2, bbox.height/2];
+    } else {
+      opos = [0,0];
+    }
 
-    return group.attr("transform", "translate(" + (env.xAxis(co[0])) + "," + (env.yAxis(co[1])) + ")");
+    console.log(bbox);
+    let scale = 1;
+
+    if (size) {
+      if (typeof size === 'number') size = [size, size/1.6];
+      size = [Math.abs(env.xAxis(size[0]) - env.xAxis(0)), Math.abs(env.yAxis(size[1]) - env.yAxis(0))];
+      console.log(size);
+      scale = size[0] / bbox.width;
+    }
+
+    
+
+    const vbox = group.node().getBBox();
+
+
+
+    const cx = vbox.x + (vbox.width / 2) ;
+    const cy = vbox.y + (vbox.height / 2) ;
+    const zx = cx - scale * cx ;
+    const zy = cy - scale * cy;
+
+    env.local.bbox = bbox;
+    env.local.opos = opos;
+    env.local.scale = scale;
+    env.local.matrix = 'matrix(' + scale + ', 0, 0, ' + scale + ', ' + zx + ', ' + zy + ')';
+
+    const xx =(   env.xAxis(pos[0]) - env.xAxis(0) ) / scale   + (bbox.width/2 - opos[0]);
+    const yy =  (   env.yAxis(pos[1]) - env.yAxis(0) ) /scale   + (bbox.height/2 - opos[1]);
+
+    return group.attr('transform', env.local.matrix + ' translate(' + xx + ',' + yy + ')');
   };
 
   g2d.Inset.update = async (args, env) => {
-    const co = await interpretate(args[1], env);
+    const pos = await interpretate(args[1], env);
 
-    env.local.group.attr("transform", "translate(" + (env.xAxis(co[0])) + "," + (env.yAxis(co[1])) + ")");
-    return env.local.group;
+    const xx =(   env.xAxis(pos[0]) - env.xAxis(0) ) / env.local.scale   + (env.local.bbox.width/2 - env.local.opos[0]);
+    const yy =  (   env.yAxis(pos[1]) - env.yAxis(0) ) /env.local.scale   + (env.local.bbox.height/2 - env.local.opos[1]);
+
+    return env.local.group.attr('transform', env.local.matrix + ' translate(' + xx + ',' + yy + ')');
+   
   };
 
   g2d.Inset.destroy = async (args, env) => {
